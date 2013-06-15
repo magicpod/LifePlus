d7c1c9e3cb2a1ac91348377f4a98b1d1f3f61332
fukuda <fukuda.hiroyuki@tpj.co.jp>
2013-06-15 18:34:12 +0900

    fixed twitter login

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :trackable, :omniauthable
+
+  # Setup accessible (or protected) attributes for your model
+  # attr_accessible :email, :password, :password_confirmation, :remember_me
+  # # attr_accessible :title, :body
+  attr_accessible :name, :password, :uid, :provider
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+      end
+    end
+  end
+
+  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.info.nickname,
+                          provider:auth.provider,
+                          uid:auth.uid,
+#                          email:auth.extra.user_hash.email, #色々頑張りましたがtwitterではemail取得できません
+                          password:Devise.friendly_token[0,20]
+                          )
+    end
+    user
+  end
+
+end

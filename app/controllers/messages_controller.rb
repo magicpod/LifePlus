7432cd720a0520c4ce084d9962d4272bac2e7ea2
fukuda <fukuda.hiroyuki@tpj.co.jp>
2013-06-16 03:20:17 +0900

    fixed twitter DM

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -103,17 +103,6 @@ class MessagesController < ApplicationController
   def tweet
 
     @messages.each do |message|
-      Twitter.configure do |config|
-        config.consumer_key = 'galtGPSTwyL8gvnMJlzbg'
-        config.consumer_secret = 'osmPS76ML9mkLut5O2Ybz6q9QigvAOOZYSZzNGyN4'
-        config.oauth_token = '1518978194-olqwtfG285noLBpq60Vp4S3AhD2CFkJ6fQ1Y3Ow'
-        config.oauth_token_secret = 'TlvxDGu1FuVvJiiGuw0JYdyA6NAwK24WUgs7A7zrSo'
-      end
-
-      content = message.created_at.strftime("%Y年%m月%d日 %H:%M:%S")
-      content << "\r\n"
-      content << "タイムカプセルを掘り起こす時が来ました。"
-      Twitter.update( content )
 
       Twitter.configure do |config|
         config.consumer_key = 'galtGPSTwyL8gvnMJlzbg'
@@ -121,6 +110,12 @@ class MessagesController < ApplicationController
         config.oauth_token = message.user.access_token
         config.oauth_token_secret = message.user.access_secret
       end
+
+      content = message.created_at.now.strftime("%Y年%m月%d日 %H:%M:%S")
+      content << "\r\n"
+      content << "タイムカプセルを掘り起こす時が来ました。"
+      Twitter.direct_message_create( message.user.name, content )
+
     end
 
   end
@@ -138,7 +133,7 @@ class MessagesController < ApplicationController
     users.each do |user|
       options = {"since_id" => 1, "include_entities" => true}
       Twitter.user_timeline( user.name, options ).each do |res|
-        # puts res.inspect
+        # puts res.text
         # Message.create( :user_id => user.id, :content => res.text, :notice_date => DateTime.now, :noticed => true )
         # break
       end
